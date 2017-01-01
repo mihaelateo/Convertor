528084125c0e94de044e2877d35025b1ea8fffd7
mihaelateo <mihaelateo@users.noreply.github.com>
2017-01-01 19:46:57 +0200

    Main menu

---
+++ converter.cpp
@@ -0,0 +1,26 @@
+#include "converter.h"
+
+int main()
+{
+	cout << "~~~UNIT CONVERTER~~~" << endl; 
+	cout << " Tipuri de conversie: " << endl;
+	
+	cout << " Simple units: " << endl;
+	cout << " 1. Arie " << endl;
+	cout << " 2. Lungime " << endl;
+	cout << " 3. Volum " << endl;
+	cout << " 4. Timp " << endl;
+	cout << " 5. Temperatura " << endl;
+	cout << " 6. Masa " << endl;
+	cout << " 7. Energie " << endl;
+	cout << " 8. Presiune " << endl;
+	cout << " 9. Densitate" << endl;
+
+	cout << " Combinational units: " << endl;
+	cout << " 10. Viteza " << endl;
+	cout << " 11. Consum combustibil " << endl;
+	
+
+
+	return 0;
+}
